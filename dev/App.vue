12b1ef304592d72d7e47bc9e430966db3785978a
surmon-china <surmon.me@gmail.com>
2022-08-27 20:38:18 +0800

    chore: format

--- dev/App.vue
+++ dev/App.vue
@@ -1,111 +1,111 @@
 <script setup lang="ts">
-import { reactive, shallowRef, computed, onMounted } from 'vue'
-import { javascript } from '@codemirror/lang-javascript'
-import { html } from '@codemirror/lang-html'
-import { json } from '@codemirror/lang-json'
-import { markdown } from '@codemirror/lang-markdown'
-import { oneDark } from '@codemirror/theme-one-dark'
-import { Codemirror } from '../src'
+  import { reactive, shallowRef, computed, onMounted } from 'vue'
+  import { javascript } from '@codemirror/lang-javascript'
+  import { html } from '@codemirror/lang-html'
+  import { json } from '@codemirror/lang-json'
+  import { markdown } from '@codemirror/lang-markdown'
+  import { oneDark } from '@codemirror/theme-one-dark'
+  import { Codemirror } from '../src'
 
-const themes: any = { oneDark }
-const languages: any = {
-  javascript: javascript(),
-  html: html(),
-  json: json(),
-  markdown: markdown()
-}
-
-const consoleLog = console.log
-const code = shallowRef(`console.log('Hello World')`)
-const view = shallowRef()
-const state = reactive({
-  disabled: false,
-  indentWithTab: true,
-  tabSize: 4,
-  autofocus: true,
-  placeholder: 'input...',
-  backgroundColor: 'red',
-  language: 'javascript',
-  theme: 'oneDark',
-  phrases: 'en-us'
-})
-
-const handleReady = (payload: any) => {
-  console.log('handleReady payload:', payload)
-}
-
-const extensions = computed(() => {
-  const result = []
-  result.push(languages[state.language])
-  if (themes[state.theme]) {
-    result.push(themes[state.theme])
+  const themes: any = { oneDark }
+  const languages: any = {
+    javascript: javascript(),
+    html: html(),
+    json: json(),
+    markdown: markdown()
   }
-  return result
-})
 
-onMounted(() => {
-  console.log('mounted view:', view)
-})
+  const consoleLog = console.log
+  const code = shallowRef(`console.log('Hello World')`)
+  const view = shallowRef()
+  const config = reactive({
+    disabled: false,
+    indentWithTab: true,
+    tabSize: 4,
+    autofocus: true,
+    placeholder: 'input...',
+    backgroundColor: 'red',
+    language: 'javascript',
+    theme: 'oneDark',
+    phrases: 'en-us'
+  })
 
-const germanPhrases = {
-  // @codemirror/view
-  'Control character': 'Steuerzeichen',
-  // @codemirror/commands
-  'Selection deleted': 'Auswahl gelöscht',
-  // @codemirror/language
-  'Folded lines': 'Eingeklappte Zeilen',
-  'Unfolded lines': 'Ausgeklappte Zeilen',
-  to: 'bis',
-  'folded code': 'eingeklappter Code',
-  unfold: 'ausklappen',
-  'Fold line': 'Zeile einklappen',
-  'Unfold line': 'Zeile ausklappen',
-  // @codemirror/search
-  'Go to line': 'Springe zu Zeile',
-  go: 'OK',
-  Find: 'Suchen',
-  Replace: 'Ersetzen',
-  next: 'nächste',
-  previous: 'vorherige',
-  all: 'alle',
-  'match case': 'groß/klein beachten',
-  'by word': 'ganze Wörter',
-  replace: 'ersetzen',
-  'replace all': 'alle ersetzen',
-  close: 'schließen',
-  'current match': 'aktueller Treffer',
-  'replaced $ matches': '$ Treffer ersetzt',
-  'replaced match on line $': 'Treffer on Zeile $ ersetzt',
-  'on line': 'auf Zeile',
-  // @codemirror/autocomplete
-  Completions: 'Vervollständigungen',
-  // @codemirror/lint
-  Diagnostics: 'Diagnosen',
-  'No diagnostics': 'Keine Diagnosen'
-}
+  const handleReady = (payload: any) => {
+    console.log('handleReady payload:', payload)
+  }
+
+  const extensions = computed(() => {
+    const result = []
+    result.push(languages[config.language])
+    if (themes[config.theme]) {
+      result.push(themes[config.theme])
+    }
+    return result
+  })
+
+  onMounted(() => {
+    console.log('mounted view:', view)
+  })
+
+  const germanPhrases = {
+    // @codemirror/view
+    'Control character': 'Steuerzeichen',
+    // @codemirror/commands
+    'Selection deleted': 'Auswahl gelöscht',
+    // @codemirror/language
+    'Folded lines': 'Eingeklappte Zeilen',
+    'Unfolded lines': 'Ausgeklappte Zeilen',
+    to: 'bis',
+    'folded code': 'eingeklappter Code',
+    unfold: 'ausklappen',
+    'Fold line': 'Zeile einklappen',
+    'Unfold line': 'Zeile ausklappen',
+    // @codemirror/search
+    'Go to line': 'Springe zu Zeile',
+    go: 'OK',
+    Find: 'Suchen',
+    Replace: 'Ersetzen',
+    next: 'nächste',
+    previous: 'vorherige',
+    all: 'alle',
+    'match case': 'groß/klein beachten',
+    'by word': 'ganze Wörter',
+    replace: 'ersetzen',
+    'replace all': 'alle ersetzen',
+    close: 'schließen',
+    'current match': 'aktueller Treffer',
+    'replaced $ matches': '$ Treffer ersetzt',
+    'replaced match on line $': 'Treffer on Zeile $ ersetzt',
+    'on line': 'auf Zeile',
+    // @codemirror/autocomplete
+    Completions: 'Vervollständigungen',
+    // @codemirror/lint
+    Diagnostics: 'Diagnosen',
+    'No diagnostics': 'Keine Diagnosen'
+  }
 </script>
 
 <template>
   <div class="example">
     <div class="toolbar">
-      <pre class="state">{{ state }}</pre>
+      <pre class="state">{{ config }}</pre>
       <div class="config">
         <p>
-          <button @click="state.disabled = !state.disabled">toggle disabled</button>
+          <button @click="config.disabled = !config.disabled">toggle disabled</button>
         </p>
         <p>
-          <button @click="state.autofocus = !state.autofocus">toggle autofocus</button>
+          <button @click="config.autofocus = !config.autofocus">toggle autofocus</button>
         </p>
         <p>
-          <button @click="state.indentWithTab = !state.indentWithTab">toggle indentWithTab</button>
+          <button @click="config.indentWithTab = !config.indentWithTab">toggle indentWithTab</button>
         </p>
         <p>
           <label for="tabSize">tabSize:</label>
-          <input type="range" id="tabSize" min="2" max="10" step="2" v-model.number="state.tabSize" />
+          <input type="range" id="tabSize" min="2" max="10" step="2" v-model.number="config.tabSize" />
         </p>
         <p>
           <label for="backgroundColor">backgroundColor:</label>
-          <select name="backgroundColor" id="backgroundColor" v-model="state.backgroundColor">
+          <select name="backgroundColor" id="backgroundColor" v-model="config.backgroundColor">
             <option
               :value="option"
               :key="option"
@@ -117,7 +117,7 @@ const germanPhrases = {
         </p>
         <p>
           <label for="language">language:</label>
-          <select name="language" id="language" v-model="state.language">
+          <select name="language" id="language" v-model="config.language">
             <option
               :value="option"
               :key="option"
@@ -129,7 +129,7 @@ const germanPhrases = {
         </p>
         <p>
           <label for="theme">theme:</label>
-          <select name="theme" id="theme" v-model="state.theme">
+          <select name="theme" id="theme" v-model="config.theme">
             <option :value="option" :key="option" v-for="option in ['default', 'oneDark']">
               {{ option }}
             </option>
@@ -137,7 +137,7 @@ const germanPhrases = {
         </p>
         <p>
           <label for="phrases">phrases:</label>
-          <select name="phrases" id="phrases" v-model="state.phrases">
+          <select name="phrases" id="phrases" v-model="config.phrases">
             <option :value="option" :key="option" v-for="option in ['en-us', 'de-de']">
               {{ option }}
             </option>
@@ -150,14 +150,14 @@ const germanPhrases = {
       <codemirror
         class="codemirror"
         ref="cm"
-        :autofocus="state.autofocus"
-        :placeholder="state.placeholder"
-        :indentWithTab="state.indentWithTab"
-        :tabSize="state.tabSize"
-        :disabled="state.disabled"
-        :style="{ backgroundColor: state.backgroundColor }"
+        :autofocus="config.autofocus"
+        :placeholder="config.placeholder"
+        :indentWithTab="config.indentWithTab"
+        :tabSize="config.tabSize"
+        :disabled="config.disabled"
+        :style="{ backgroundColor: config.backgroundColor }"
         :extensions="extensions"
-        :phrases="state.phrases === 'en-us' ? {} : germanPhrases"
+        :phrases="config.phrases === 'en-us' ? {} : germanPhrases"
         v-model="code"
         @ready="handleReady"
         @change="consoleLog('change', $event)"
@@ -169,47 +169,47 @@ const germanPhrases = {
 </template>
 
 <style lang="scss">
-body {
-  margin: 0;
-}
-
-.example {
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  .toolbar {
-    display: flex;
-    align-items: center;
-    margin-bottom: 1rem;
-
-    .state {
-      margin: 2rem 0;
-      margin-right: 2rem;
-      padding: 2em;
-      border: 1px solid #ccc;
-    }
+  body {
+    margin: 0;
   }
 
-  .content {
+  .example {
+    width: 100vw;
+    height: 100vh;
     display: flex;
-    width: 100%;
+    flex-direction: column;
     justify-content: center;
+    align-items: center;
 
-    .code {
-      overflow: scroll;
+    .toolbar {
+      display: flex;
+      align-items: center;
+      margin-bottom: 1rem;
+
+      .state {
+        margin: 2rem 0;
+        margin-right: 2rem;
+        padding: 2em;
+        border: 1px solid #ccc;
+      }
     }
 
-    .code,
-    .codemirror .cm-editor {
-      width: 30vw;
-      height: 50vh;
-      margin: 0 1rem;
-      border: 1px solid #ddd;
+    .content {
+      display: flex;
+      width: 100%;
+      justify-content: center;
+
+      .code {
+        overflow: scroll;
+      }
+
+      .code,
+      .codemirror .cm-editor {
+        width: 30vw;
+        height: 50vh;
+        margin: 0 1rem;
+        border: 1px solid #ddd;
+      }
     }
   }
-}
 </style>
